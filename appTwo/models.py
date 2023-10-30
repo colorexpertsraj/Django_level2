619513cc71c05eb9fe40955f38e8c27c858fd121
Md Abdur Rajjak <colorexpertsraj@gmail.com>
2023-10-30 10:02:34 +0600

    level two

---
+++ appTwo/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    email = models.EmailField(max_length=264,unique=True)
+
+
+
+# python manage.py createsuperuser
+
+# Username: Rajjak
+
+# Email address: colorexpertsraj@gmail.com
+
+# Password: Rajjak
